893dba5e241374f3f4fb0082b2d8bbdddbbc7c2f
Aljoscha Meyer <mail@aljoscha-meyer.de>
2022-11-14 22:15:25 +0100

    Meet smoothy

---
+++ src/smoothy.rs
@@ -0,0 +1,57 @@
+// Disclaimer:
+// - Beyond an educated guess that samples might denote an amplitude between -1 to 1, I have no idea how any of this works.
+// - I have not listened to what this produces. The initial idea was to make things sluggish by limiting how much samples would differ from the previous ones. But perhaps all this does is to reduce volume in a slightly random way? Only one way to find out: submitting a PR and having you describe the effect to me.
+
+use std::env;
+use std::io;
+use std::collections::VecDeque;
+
+// Smooth things out by averaging every sample over past samples.
+
+// Maintain a buffer of the last `n` samples, adding `sample` as the newest member at index 0.
+fn feed_blender(buf: &mut VecDeque<f64>, sample: f64, n: usize) {
+    if buf.len() == n {
+        buf.pop_back();
+    }
+    buf.push_front(sample);
+}
+
+fn main() {
+    // Parse args or set defaults
+    let args: Vec<String> = env::args().collect();
+
+    // How long to leave the smoothie in the blender (how many past samples to average over).
+    let blending_time: usize = match args.get(1) {
+        Some(ms) => ms.parse().map(|n: usize| n + 1).unwrap_or(1),
+        None => 1,
+    };
+
+    let stdin = io::stdin();
+
+    let mut lines = stdin.lines();
+    let mut sample: f64 = 0.0;
+
+    let mut buf = VecDeque::new();    
+
+    // Infinite loop over samples passed via stdin
+    loop {
+        let line = lines.next();
+        sample = match line {
+            Some(line) => match line.expect("can read line").parse::<f64>() {
+                Ok(sample) => {
+                    feed_blender(&mut buf, sample, blending_time);
+                    let mut smoothed = 0.0;
+                    for i in 0..blending_time {
+                        smoothed += buf.get(i).map(Clone::clone).unwrap_or(sample);
+                    }
+                    smoothed / (blending_time as f64)
+                }
+                Err(_) => sample,
+            },
+            None => sample,
+        };
+
+        // print the smoothed sample to stdout
+        println!("{sample}")
+    }
+}
