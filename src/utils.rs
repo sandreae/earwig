5efc2574a29a73d6665686d43312d7a83019e34f
Sam Andreae <contact@samandreae.com>
2022-11-18 09:24:58 +0000

    Formatting

--- src/utils.rs
+++ src/utils.rs
@@ -11,9 +11,7 @@ macro_rules! sample_loop {
     ($sample:pat in $body:block) => {
         for raw in std::io::stdin().lines() {
             match raw.expect("Can read line").parse::<f64>() {
-                Ok($sample) => {
-                    $body
-                }
+                Ok($sample) => $body,
                 Err(_) => continue, // We forgivingly ignore input that does not parse as a float.
                                     // Neither do we enforce that float to lie between -1 and 1 btw.
             }
@@ -26,11 +24,9 @@ pub fn next_sample(lines: &mut Lines<StdinLock>) -> Option<f64> {
     let line = lines.next();
 
     match line {
-        Some(line) => {
-            match line.expect("Can read line").parse() {
-                Ok(sample) => Some(sample),
-                Err(_) => None,
-            }
+        Some(line) => match line.expect("Can read line").parse() {
+            Ok(sample) => Some(sample),
+            Err(_) => None,
         },
         None => None,
     }
@@ -38,5 +34,5 @@ pub fn next_sample(lines: &mut Lines<StdinLock>) -> Option<f64> {
 
 // https://en.wikipedia.org/wiki/Linear_interpolation#Programming_language_support
 pub fn lerp(v0: f64, v1: f64, t: f64) -> f64 {
-    return (1.0 - t) * v0 + t * v1;
+    (1.0 - t) * v0 + t * v1
 }
