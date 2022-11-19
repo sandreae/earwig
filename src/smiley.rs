c866615f5680f30928d7fbb160e392da9ffa6a64
Aljoscha Meyer <mail@aljoscha-meyer.de>
2022-11-19 22:48:27 +0100

    Slightly improve some modules

--- src/smiley.rs
+++ src/smiley.rs
@@ -1,5 +1,4 @@
 // Always stays positive :-)
-// (except for the cynic)
 
 use std::env;
 
@@ -12,14 +11,11 @@ fn main() {
     let is_manic = args.get(1).map(|arg| arg == "manic").unwrap_or(false);
     let is_cynic = args.get(1).map(|arg| arg == "cynic").unwrap_or(false);
 
-    let mut previous_sample: f64 = 1.0;
-
     sample_loop![sample in {
         if is_manic {
             println!("{}", sample.abs());
         } else if is_cynic {
-            println!("{}", sample * previous_sample);
-            previous_sample = sample;
+            println!("{}", sample * sample);
         } else {
             println!("{}", sample.max(0.0));
         }
